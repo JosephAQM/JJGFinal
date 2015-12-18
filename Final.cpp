64be891a25ce6d21ca947e1db7ea83e63be01fb2
John Zhang <john.zhang.gyz@gmail.com>
2015-12-17 22:28:04 -0500

    Room is now scaled

--- Final.cpp
+++ Final.cpp
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 float angle = 0.0f;
 float ver[8][3] = 
 {
@@ -36,32 +37,30 @@ GLfloat color[8][3] =
 
 void quad(int a,int b,int c,int d,float scaleX,float scaleY,float scaleZ )
 {
-	int i;
-    glBegin(GL_QUADS);
-    
-    	glColor3fv(color[a]);
-    	glVertex3f(ver[a][0]*scaleX,ver[a][1]*scaleY,ver[a][2]*scaleZ);
-
-    	glColor3fv(color[b]);
-	glVertex3f(ver[b][0]*scaleX,ver[b][1]*scaleY,ver[b][2]*scaleZ);
-
-	glColor3fv(color[c]);
-	glVertex3f(ver[c][0]*scaleX,ver[c][1]*scaleY,ver[c][2]*scaleZ);
-
-	glColor3fv(color[d]);
-	glVertex3f(ver[d][0]*scaleX,ver[d][1]*scaleY,ver[d][2]*scaleZ);
-
-    glEnd();
+	glBegin(GL_QUADS);
+		glColor3fv(color[a]);
+		glVertex3f(ver[a][0]*scaleX,ver[a][1]*scaleY,ver[a][2]*scaleZ);
+		
+		glColor3fv(color[b]);
+		glVertex3f(ver[b][0]*scaleX,ver[b][1]*scaleY,ver[b][2]*scaleZ);
+		
+		glColor3fv(color[c]);
+		glVertex3f(ver[c][0]*scaleX,ver[c][1]*scaleY,ver[c][2]*scaleZ);
+		
+		glColor3fv(color[d]);
+		glVertex3f(ver[d][0]*scaleX,ver[d][1]*scaleY,ver[d][2]*scaleZ);
+	
+	glEnd();
 }
 
 void room1()
 {
-    quad(0,3,2,1,5,5,5);
-    quad(2,3,7,6,5,5,5);
-    quad(0,4,7,3,5,5,5);
-    quad(1,2,6,5,5,5,5);
-    quad(4,5,6,7,5,5,5);
-    quad(0,1,5,4,5,5,5);
+    quad(0,3,2,1,10,5,11);
+    quad(2,3,7,6,10,5,11);
+    quad(0,4,7,3,10,5,11);
+    quad(1,2,6,5,10,5,11);
+    quad(4,5,6,7,10,5,11);
+    quad(0,1,5,4,10,5,11);
 }
 
 void changeSize(int w, int h) { 
@@ -117,7 +116,7 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-windowSizeWidth)/2,
-                      	   (glutGet(GLUT_SCREEN_HEIGHT)-windowSizeHeight*1.3)/2); //initial window position is centered and slightly up(cuz laptop screen so I dont have to look down)
+                      	       (glutGet(GLUT_SCREEN_HEIGHT)-windowSizeHeight*1.3)/2); //initial window position is centered and slightly up(cuz laptop screen so I dont have to look down)
 	glutInitWindowSize(windowSizeWidth,windowSizeHeight);
 	glutCreateWindow("Unblock3D - 3GC3 Final");
 
