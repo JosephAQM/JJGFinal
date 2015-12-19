ae8926149d6f1337318ef5f842fe77d252a45ae0
John Zhang <john.zhang.gyz@gmail.com>
2015-12-19 17:06:18 -0500

    Added Torch Shapes
    Also disabled culling for the shapes ***Might also need to do for joseph's blocks

--- Unblock3D.cpp
+++ Unblock3D.cpp
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <vector>
 //-----------------------------Things for movement-----------------------------------------------
 //angle of rotation
 float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle=0.0;
@@ -221,11 +222,79 @@ public:
 
 };
 
+class SolidSphere
+{
+protected:
+    std::vector<GLfloat> vertices;
+    std::vector<GLfloat> normals;
+    std::vector<GLfloat> texcoords;
+    std::vector<GLushort> indices;
+
+public:
+    SolidSphere(float radius, unsigned int rings, unsigned int sectors)
+    {
+        float const R = 1./(float)(rings-1);
+        float const S = 1./(float)(sectors-1);
+        int r, s;
+
+        vertices.resize(rings * sectors * 3);
+        normals.resize(rings * sectors * 3);
+        texcoords.resize(rings * sectors * 2);
+        std::vector<GLfloat>::iterator v = vertices.begin();
+        std::vector<GLfloat>::iterator n = normals.begin();
+        std::vector<GLfloat>::iterator t = texcoords.begin();
+        for(r = 0; r < rings; r++) for(s = 0; s < sectors; s++) {
+                float const y = sin( -M_PI_2 + M_PI * r * R );
+                float const x = cos(2*M_PI * s * S) * sin( M_PI * r * R );
+                float const z = sin(2*M_PI * s * S) * sin( M_PI * r * R );
+
+                *t++ = s*S;
+                *t++ = r*R;
+
+                *v++ = x * radius;
+                *v++ = y * radius;
+                *v++ = z * radius;
+
+                *n++ = x;
+                *n++ = y;
+                *n++ = z;
+        }
+
+        indices.resize(rings * sectors * 4);
+        std::vector<GLushort>::iterator i = indices.begin();
+        for(r = 0; r < rings-1; r++) for(s = 0; s < sectors-1; s++) {
+                *i++ = r * sectors + s;
+                *i++ = r * sectors + (s+1);
+                *i++ = (r+1) * sectors + (s+1);
+                *i++ = (r+1) * sectors + s;
+        }
+    }
+
+    void draw(GLfloat x, GLfloat y, GLfloat z)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslatef(x,y,z);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_NORMAL_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+        glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
+        glNormalPointer(GL_FLOAT, 0, &normals[0]);
+        glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
+        glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
+        glPopMatrix();
+    }
+};
+
+
 //-----------------------------------------------------------------------------------------------
 
 
 //----------------------------------------------- GLOBALS ---------------------------------------
 
+SolidSphere sphere(0.3, 24, 24);
 //Array of objects containing all of the blocks (arbritrary limit of 20)
 Block sceneBlocks[20];
 //Contains whether the blocks are currently being used or not
@@ -403,7 +472,39 @@ void quad(int a,int b,int c,int d,float scaleX,float scaleY,float scaleZ )
 
     glEnd();
 }
+void drawBase(float xp,float yp,float zp,float xr,float yr,float zr,float size){
+		glPushMatrix();
+		//Translate object
+		glTranslated(xp,yp,zp);
+		//Rotate Object
+		glRotatef(xr, 1.0, 0.0, 0.0);
+		glRotatef(yr, 0.0, 1.0, 0.0);
+		glRotatef(zr, 0.0, 0.0, 1.0);
+		glutSolidCone(size , size*3, 50, 50);
+		glPopMatrix(); 
+}
+void drawTorches(){
+	glDisable(GL_CULL_FACE);
+	sphere.draw(3.5, -2, 10.5);
+	drawBase(3.5,-2.2,10.6,70,0,45,0.4);
+	sphere.draw(-3.5, -2, 10.5);
+	drawBase(-3.5,-2.2,10.6,70,0,45,0.4);
 
+	sphere.draw(3.5, -2, -10.5);
+	drawBase(3.5,-2.2,-10.6,110,0,45,0.4);
+	sphere.draw(-3.5, -2, -10.5);
+	drawBase(-3.5,-2.2,-10.6,110,0,45,0.4);
+
+	sphere.draw(-9.5, -2, 5);
+	drawBase(-9.5,-2.2,5,90,-22,45,0.4);
+	sphere.draw(-9.5, -2, -5);
+	drawBase(-9.5,-2.2,-5,90,-22,45,0.4);
+
+	sphere.draw(9.5, -2, 5);
+	drawBase(9.5,-2.2,5,90,22,45,0.4);
+	sphere.draw(9.5, -2, -5);
+	drawBase(9.5,-2.2,-5,90,22,45,0.4);
+}
 void room1()
 {
 	glColor3fv(color[6]);
@@ -421,7 +522,7 @@ void room1()
 }
 
 void init(void) {
-	glEnable(GL_CULL_FACE);
+	
 	glCullFace(GL_BACK);
 	glFrontFace(GL_CW);
 	glEnable(GL_DEPTH_TEST); //enable the depth testing
@@ -455,6 +556,7 @@ void drawAllBlocks(){
 }
 
 void renderScene(void) {
+	glEnable(GL_CULL_FACE);
 	// Clear Color and Depth Buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -480,7 +582,7 @@ void renderScene(void) {
 	//glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 	room1();
 	drawAllBlocks();
-
+	drawTorches();
 	//TESTING
 	//drawTestPoints();
 	//angle+=0.5f;
